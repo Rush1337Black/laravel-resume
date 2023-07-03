1c639b7ccbbb395ce01a35315b67477e5c8f3d82
Rush <128978324+Rush1337Black@users.noreply.github.com>
2023-07-03 04:36:05 +0300

    migration
    created history passswords, emails

---
+++ database/migrations/2023_07_03_012633_create_history_passwords_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('history_passwords', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('password');
+            $table->string('description')->nullable();
+            $table->date('created_at');
+
+
+
+            $table->index('user_id','history_password_user_idx');
+            $table->foreign('user_id','history_password_user_fk')->
+            on('users')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('history_passwords');
+    }
+};
